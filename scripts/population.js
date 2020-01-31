0af50d767d248f8934ce588d81252368eab95edd
Gaetan Nicolas <gnicolas@appstud.com>
2020-01-31 10:57:09 +0100

    population

--- scripts/population.js
+++ scripts/population.js
@@ -1,7 +1,30 @@
 console.log('Good luck space cowboy !');
 
-fetch(
-  'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/population.json',
-)
-  .then(resp => resp.json())
-  .then(data => console.log('DATA', data));
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+  fetch(
+    'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/population.json'
+  )
+  .then(res => res.json())
+  .then(resData => dataViz(resData))
+  // code
+  const container = document.getElementById('container')
+
+  const dataViz = (arrayData) => {
+    arrayData.map((city) => {
+      console.log(city)
+
+      const item = document.createElement('div')
+      const p = document.createElement('p')
+      p.innerHTML = city.name
+      
+      item.setAttribute('style', `height: ${city.population / 5000}px` )
+      item.classList.add('item')
+      
+      item.appendChild(p)
+      container.appendChild(item)
+    })
+  }
+});
